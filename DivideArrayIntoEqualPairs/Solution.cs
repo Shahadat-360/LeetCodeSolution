e0bdb398157061c611d4da58ccedf6c2f9429418
MD. Shahadat Hossain Bhuian <1337.Shahadat@gmail.com>
2025-03-18 02:58:03 +0600

    divide-array-into-equal-pairs

---
+++ DivideArrayIntoEqualPairs/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivideArrayIntoEqualPairs
+{
+    public class Solution
+    {
+        public bool DivideArray(int[] nums)
+        {
+            return nums.GroupBy(x => x).All(g => g.Count() % 2 == 0);
+        }
+    }
+}
