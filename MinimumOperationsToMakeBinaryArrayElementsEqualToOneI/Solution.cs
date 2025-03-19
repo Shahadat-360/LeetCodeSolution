98b1bc15cddf84e598af6fa25b34f3cc4f1ea40c
MD. Shahadat Hossain Bhuian <1337.Shahadat@gmail.com>
2025-03-19 11:47:38 +0600

    MinimumOperationsToMakeBinaryArrayElementsEqualToOneI

---
+++ MinimumOperationsToMakeBinaryArrayElementsEqualToOneI/Solution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinimumOperationsToMakeBinaryArrayElementsEqualToOneI
+{
+    public class Solution
+    {
+        public int MinOperations(int[] nums)
+        {
+            int n = nums.Length, c = 0;
+            for (int i = 0; i <= n - 3; i++)
+            {
+                if (nums[i] == 1) continue;
+                (nums[i], nums[i + 1], nums[i + 2]) = (1, 1 - nums[i + 1], 1 - nums[i + 2]);
+                c++;
+            }
+            return nums.Sum() == n ? c : -1;
+        }
+    }
+}
