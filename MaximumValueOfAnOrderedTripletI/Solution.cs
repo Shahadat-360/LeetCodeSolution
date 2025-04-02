ba0050e7e45287a22c3bb0624ae582eb5b9ce384
MD. Shahadat Hossain Bhuian <1337.Shahadat@gmail.com>
2025-04-02 16:45:31 +0600

    maximum-value-of-an-ordered-triplet-i

---
+++ MaximumValueOfAnOrderedTripletI/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximumValueOfAnOrderedTripletI
+{
+    public class Solution
+    {
+        public long MaximumTripletValue(int[] nums)
+        {
+            long maxDiff = 0;
+            long maxk = 0;
+            long result = 0;
+            for (int k = 0; k < nums.Length; k++)
+            {
+                result = Math.Max(result, maxDiff * nums[k]);
+                maxDiff = Math.Max(maxDiff, maxk - nums[k]);
+                maxk = Math.Max(maxk, nums[k]);
+            }
+            return result;
+        }
+    }
+}
